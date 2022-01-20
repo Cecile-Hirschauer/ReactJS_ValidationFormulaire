ec94655b7c504733b58fced459b99fe7b9ace079
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-20 17:33:04 +0100

    Create indicator & cards

---
+++ src/Components/MultiForm/Infos/CardBegin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+
+function CardBegin() {
+  return (
+    <div className="card">
+      <h1>🍣 Aide-nous à ravir tes papilles 🌶️</h1>
+      <button>COMMENCER</button>
+    </div>
+  );
+}
+
+export default CardBegin;
---
+++ src/Components/MultiForm/Multiform.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import './MultiForm.css';
+import Indicator from './Indicator/Indicator';
+import CardBegin from './Infos/CardBegin';
+import CardEnd from './Infos/CardEnd';
+
+export default function Multiform() {
+
+    const [formIndex, setFormIndex] = useState(1);
+    const [allFormData, setAllFormData] = useState({
+        dietForm: "",
+        foodStyle: [],
+        allergies: [],
+        prefs: {}
+    });
+
+  return (
+  <div className='container-multiform'>
+      <Indicator />
+      {/* <CardEnd /> */}
+      <CardBegin />
+  </div>
+  );
+}
+
+
