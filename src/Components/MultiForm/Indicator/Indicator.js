62553a78c2d12db315c5e83762fc0565644ce636
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-22 17:51:58 +0100

    Display endCard and create progressive bar in indicator upper-line

--- src/Components/MultiForm/Indicator/Indicator.js
+++ src/Components/MultiForm/Indicator/Indicator.js
@@ -7,12 +7,22 @@ import thumb from './icons/thumb.svg';
 import diet from './icons/diet.svg';
 
 
-export default function Indicator() {
+export default function Indicator({formIndex}) {
+
   return (
     <div className='container-indicator'>
         <div className="container-lines">
-            <div className="line upper-line">
-
+            <div 
+            className="line upper-line"
+            style={{
+               width: formIndex === 1 ? "0%"
+                    : formIndex === 2 ? "0%"
+                    : formIndex === 3 ? "25%"
+                    : formIndex === 4 ? "50%"
+                    : formIndex === 5 ? "75%"
+                    : formIndex === 6 ? "100%"
+                    : ""
+            }}>
             </div>
             <div className="line under-line"></div>
         </div>
--- src/Components/MultiForm/Multiform.js
+++ src/Components/MultiForm/Multiform.js
@@ -31,10 +31,26 @@ export default function Multiform() {
 
   console.log(allFormData);
 
+  const elements = [
+    <CardBegin modifyIndex={modifyIndex} />,
+    <DietForm modifyIndex={modifyIndex} />,
+    <FoodStyle modifyIndex={modifyIndex} />,
+    <Allegies modifyIndex={modifyIndex} />,
+    <HateLove modifyIndex={modifyIndex} />,        
+    <CardEnd modifyIndex={modifyIndex} />
+  ]
+
   return (
     <div className="container-multiform">
-      <Indicator />
-      {formIndex === 1 ? (
+      <Indicator formIndex={formIndex}/>
+
+      {elements.map((item, index) => {
+        if ((index + 1) === formIndex) {
+          return elements[index];
+        }
+      })}
+
+      {/* {formIndex === 1 ? (
         <CardBegin modifyIndex={modifyIndex} />
       ) : formIndex === 2 ? (
         <DietForm modifyIndex={modifyIndex} />
@@ -44,9 +60,12 @@ export default function Multiform() {
         <Allegies modifyIndex={modifyIndex} />
       ):  formIndex === 5 ? (
         <HateLove modifyIndex={modifyIndex} />
+      ):  formIndex === 6 ? (
+        <CardEnd modifyIndex={modifyIndex} />
       ): (
         ""
-      )}
+      )} */}
+
     </div>
   );
 }
